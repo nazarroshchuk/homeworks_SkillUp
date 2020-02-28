b8701c2822263d24656b996c366447fed9db448b
Nazarii Roshchuk <56605370+nazarroshchuk@users.noreply.github.com>
2020-02-28 17:58:06 +0200

    Update task_loops.js

--- task_loops.js
+++ task_loops.js
@@ -10,23 +10,25 @@ let numberOfUser = +prompt("Enter your number, please!", "");
   }
 
  //2
- let i = 0;
+let i = 0;
 
-	while ( i < 3) {
-  	console.log( "номер " + i + "!");
-  	i++;
-	}
+ while ( i < 3) {
+  console.log( "номер " + i + "!");
+  i++;
+}
 
 //3
 var a = +prompt("a?", "");
 
-	switch(a) {
-	  case 0: alert(0);
-	    break;
-	  case 1: alert(1);
-	    break;
-	  case 2:
-	  case 3:
-	    alert("2, 3");
-	    break;
+switch(a) {
+  case 0: 
+    alert(0);
+    break;
+  case 1: 
+    alert(1);
+    break;
+  case 2:
+  case 3:
+   alert("2, 3");
+   break;
 	}
